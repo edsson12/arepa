71c0570d59ea9d8af1406d321ff3ea01e587a730
edsson12 <edsson.cortes@gmail.com>
2022-02-15 13:47:33 -0500

    Creacion de navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavArepa from './components/NavArepa/NavArepa'
+
+const App = () => {
+  return (
+    <>
+    <NavArepa/>
+    <div style={{ height:"1800px"}}></div> 
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavArepa/NavArepa.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import "./NavArepa.scss";
+import imgNav from '../../img/ArepaehuevoLOGO.png'
+
+const NavArepa = () => {
+  const [navColorScroll, setNavColorScroll] = useState(false);
+
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setNavColorScroll(true);
+    } else {
+      setNavColorScroll(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
+
+  return (
+    <nav className={navColorScroll ? "o-nav-arepa scrolled" : "o-nav-arepa"}>
+      <div className="o-nav-right">
+        <a href="">Acerca</a>
+        <a href="">Productos</a>
+      </div>
+
+      <img src={imgNav} alt="Arepa e huevo logo png" />
+
+      <div className="o-nav-left">
+        <a href="">Galeria</a>
+        <a href="">Otra</a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavArepa;
