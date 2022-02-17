a29bb0855f0be9856c89c6f4f3499f17067f867f
edsson12 <edsson.cortes@gmail.com>
2022-02-17 15:38:25 -0500

    re agrego barra lateral para el mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Acerca from './components/Acerca/Acerca'
+import Menu from './components/Menu/Menu'
+
 import NavArepa from './components/NavArepa/NavArepa'
 
+
 const App = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <>
-    <NavArepa/>
+    <NavArepa
+    setMenuOpen={setMenuOpen}
+    menuOpen={menuOpen}
+    />
+
+ 
+
+    <Menu
+    setMenuOpen={setMenuOpen}
+    menuOpen={menuOpen}
+    />
     <Acerca/>
+
+    
 
     <div style={{ height:"1800px"}}></div> 
 
--- src/components/NavArepa/NavArepa.jsx
+++ src/components/NavArepa/NavArepa.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { useState } from "react";
 import { ImFacebook } from "react-icons/im";
 import { BsInstagram } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
+
+
+
+
+
+
 import "./NavArepa.scss";
 import imgNav from "../../img/ArepaehuevoLOGO.png";
 
-const NavArepa = () => {
+const NavArepa = ({ menuOpen, setMenuOpen }) => {
+
+
   const [navColorScroll, setNavColorScroll] = useState(false);
 
   const changeNavbarColor = () => {
@@ -18,25 +28,44 @@ const NavArepa = () => {
   window.addEventListener("scroll", changeNavbarColor);
 
   return (
-    <nav className={navColorScroll ? "o-nav-arepa scrolled" : "o-nav-arepa"}>
-      <div className="o-img-nav-container">
-      <img src={imgNav} alt="Arepa e huevo logo png" />
-      </div>
-      
-      <div className="o-nav-right">
-        <a href="">Nosotros</a>
-        <a href="">Menú</a>
-        <a href="">Contacto</a>
-      </div>
+    <>
+      <nav className={navColorScroll ? "o-nav-arepa scrolled" : "o-nav-arepa"}>
+        <div className="o-img-nav-container">
+          <img src={imgNav} alt="Arepa e huevo logo png" />
+        </div>
 
-      <div className="o-nav-left">
-        <div className="o-logo-container">
-     
-     <ImFacebook color={navColorScroll ? 'white': '#090a0a' }></ImFacebook>
-     <BsInstagram color={navColorScroll ? 'white': '#090a0a'}></BsInstagram>
-     </div>
-      </div>
-    </nav>
+        <div className="o-nav-right">
+          <a href="">Nosotros</a>
+          <a href="">Menú</a>
+          <a href="">Contacto</a>
+        </div>
+
+        <div className="o-nav-left">
+          <div className="o-logo-container">
+            <ImFacebook
+              color={navColorScroll ? "white" : "#090a0a"}
+            ></ImFacebook> 
+            <BsInstagram
+              color={navColorScroll ? "white" : "#090a0a"}
+            ></BsInstagram>
+
+          </div>
+          
+         
+         <div className={menuOpen ? "hamburger_activo":"hamburger"} onClick={() => setMenuOpen(!menuOpen)}>
+
+         
+            {menuOpen  ? <ImCross size={"1.6rem"} color={"#f9ae19"} />:  <GiHamburgerMenu size={"2rem"} color={"#f9ae19"}/>}
+
+            
+
+
+           
+              
+            </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
